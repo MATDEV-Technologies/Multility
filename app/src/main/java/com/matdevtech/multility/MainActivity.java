daa0a7d01c44d3558cf9a6b68c13e56479c542bc
MichaelSDavid <349612929@gapps.yrdsb.ca>
2020-07-05 17:45:21 -0400

    Fix format on comment

--- app/src/main/java/com/matdevtech/multility/MainActivity.java
+++ app/src/main/java/com/matdevtech/multility/MainActivity.java
@@ -17,8 +17,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+        // Passing each menu ID as a set of IDs because each
+        // menu should be considered as top level destinations
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
