9f49ec551c75cd60aef1ca25dcb8637e6ebdd637
MichaelSDavid <349612929@gapps.yrdsb.ca>
2020-07-14 22:01:35 -0400

    Commented out some unused imports

--- app/src/main/java/com/matdevtech/multility/Notepad.java
+++ app/src/main/java/com/matdevtech/multility/Notepad.java
@@ -23,16 +23,16 @@ import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.Toast;
 import org.jetbrains.annotations.NotNull;
-import java.io.BufferedReader;
+//import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileInputStream;
+//import java.io.FileInputStream;
 import java.io.IOException;
-// import android.widget.*;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+//import android.widget.*;
+//import java.io.InputStream;
+//import java.io.InputStreamReader;
+//import java.io.OutputStreamWriter;
 import java.util.Objects;
 
 /**
